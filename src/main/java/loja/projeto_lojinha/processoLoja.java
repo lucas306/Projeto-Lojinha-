36be3ab8a1528e853639faa75112685612a4e187
Lucas Jarandia <lucas.jarandia@TDSINFORMATICA.COM.BR>
2022-08-17 15:34:29 -0300

    Alteração na classe Processos loja

--- src/main/java/loja/projeto_lojinha/processoLoja.java
+++ src/main/java/loja/projeto_lojinha/processoLoja.java
@@ -7,30 +7,29 @@ import javax.swing.JOptionPane;
 
 public class processoLoja {
 
-    private int codigo;
-    
-    private String fone;
-    private String email;
-    
     private int ID;
     private String nome;
     private int CPF;
     private String Setor;
-    
     private int ID_Produto;
     private String nome_produto;
     private double valor;
     private String descricao;
+    private String cargo;
     
     
-    public processoLoja(String email, String fone, String nome, int codigo,
-     int ID, int CPF, String Setor, int ID_Produto, String nome_produto,
-     double valor, String descricao
+    public processoLoja(
+        String nome, 
+        int codigo,
+        int ID, int CPF, 
+        String Setor, 
+        int ID_Produto, 
+        String nome_produto,
+        double valor, 
+        String descricao,
+        String cargo
     ){
-        this.codigo = codigo;
-        this.email = email;
-        this.fone = fone;
-        
+  
         this.nome = nome;
         this.CPF = CPF;
         this.ID = ID;
@@ -39,8 +38,7 @@ public class processoLoja {
         this.nome_produto = nome_produto;
         this.Setor = Setor;
         this.valor = valor;
-        
-        
+        this.cargo = cargo;
     }
 
     public processoLoja() {}
@@ -52,7 +50,7 @@ public class processoLoja {
     
     public void inserirUser() {
         //1: Definir o comando SQL
-        String sql = "INSERT INTO usuarios(nome, CPF, Setor) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO usuarios(nome, CPF, Setor, cargo) VALUES (?, ?, ?, ?)";
         //2: Abrir uma conexão
         ConnectionFactory factory = new ConnectionFactory();
         try ( Connection c = factory.obtemConexao()) {
@@ -62,6 +60,7 @@ public class processoLoja {
             ps.setString(1, nome);
             ps.setInt(2, CPF);
             ps.setString(3, Setor);
+            ps.setString(4, cargo);
             //5: Executa o comando
             ps.execute();
         } catch (Exception e) {
@@ -72,7 +71,7 @@ public class processoLoja {
 
     public void atualizarUser() {
         //1: Definir o comando SQL
-        String sql = "UPDATE usuarios SET nome = ?, CPF = ?, Setor = ? WHERE ID = ?";
+        String sql = "UPDATE usuarios SET nome = ?, CPF = ?, Setor = ?, cargo = ? WHERE ID = ?";
         //2: Abrir uma conexão
         ConnectionFactory factory = new ConnectionFactory();
         try ( Connection c = factory.obtemConexao()) {
@@ -80,9 +79,10 @@ public class processoLoja {
             PreparedStatement ps = c.prepareStatement(sql);
             //4: Preenche os dados faltantes
             ps.setString(1, nome);
-            ps.setString(2, CPF);
-            ps.setString(3, email);
-            ps.setInt(4, codigo);
+            ps.setInt(2, CPF);
+            ps.setString(3, Setor);
+            ps.setString(4, cargo);
+            ps.setInt(5, ID);
             //5: Executa o comando
             ps.execute();
         } catch (Exception e) {
@@ -99,7 +99,7 @@ public class processoLoja {
             //3: Pré compila o comando
             PreparedStatement ps = c.prepareStatement(sql);
             //4: Preenche os dados faltantes
-            ps.setInt(1, codigo);
+            ps.setInt(1, ID);
             //5: Executa o comando
             ps.execute();
         } catch (Exception e) {
@@ -120,39 +120,41 @@ public class processoLoja {
             ResultSet rs = ps.executeQuery();
             //5: itera sobre o resultado
             while (rs.next()) {
-                int codigo = rs.getInt("codigo");
+                int ID = rs.getInt("ID");
                 String nome = rs.getString("nome");
-                String fone = rs.getString("fone");
-                String email = rs.getString("email");
+                String CPF = rs.getString("CPF");
+                String Setor = rs.getString("Setor");
                 String aux = String.format(
-                        "Código: %d, Nome: %s, Fone: %s, Email: %s",
-                        codigo,
+                        "ID: %d, NOME: %s, CPF: %s, SETOR: %s",
+                        ID,
                         nome,
-                        fone,
-                        email
+                        CPF,
+                        Setor
                 );
                 JOptionPane.showMessageDialog(null, aux);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        /**
-         * 
-         * Parte de produtos
-         */
-        
-        public void inserirProduto() {
+    }
+    
+    /**
+     *
+     * Parte de produtos
+     */
+                
+    public void inserirProduto() {
         //1: Definir o comando SQL
-        String sql = "INSERT INTO Produtos(nome, fone, email) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO Produtos(nome_produto, valor, descricao) VALUES (?, ?, ?)";
         //2: Abrir uma conexão
         ConnectionFactory factory = new ConnectionFactory();
         try ( Connection c = factory.obtemConexao()) {
             //3: Pré compila o comando
             PreparedStatement ps = c.prepareStatement(sql);
             //4: Preenche os dados faltantes
-            ps.setString(1, nome);
-            ps.setString(2, fone);
-            ps.setString(3, email);
+            ps.setString(1, nome_produto);
+            ps.setDouble(2, valor);
+            ps.setString(3, descricao);
             //5: Executa o comando
             ps.execute();
         } catch (Exception e) {
@@ -163,17 +165,17 @@ public class processoLoja {
 
     public void atualizarProduto() {
         //1: Definir o comando SQL
-        String sql = "UPDATE Produtos SET nome = ?, fone = ?, email = ? WHERE codigo = ?";
+        String sql = "UPDATE Produtos SET nome_produto = ?, valor = ?, descricao = ? WHERE ID_Produto = ?";
         //2: Abrir uma conexão
         ConnectionFactory factory = new ConnectionFactory();
         try ( Connection c = factory.obtemConexao()) {
             //3: Pré compila o comando
             PreparedStatement ps = c.prepareStatement(sql);
             //4: Preenche os dados faltantes
-            ps.setString(1, nome);
-            ps.setString(2, fone);
-            ps.setString(3, email);
-            ps.setInt(4, codigo);
+            ps.setString(1, nome_produto);
+            ps.setDouble(2, valor);
+            ps.setString(3, descricao);
+            ps.setInt(4, ID_Produto);
             //5: Executa o comando
             ps.execute();
         } catch (Exception e) {
@@ -183,14 +185,14 @@ public class processoLoja {
 
     public void apagarProduto() {
         //1: Definir o comando SQL
-        String sql = "DELETE FROM Produtos WHERE codigo = ?";
+        String sql = "DELETE FROM Produtos WHERE ID_Produto = ?";
         //2: Abrir uma conexão
         ConnectionFactory factory = new ConnectionFactory();
         try ( Connection c = factory.obtemConexao()) {
             //3: Pré compila o comando
             PreparedStatement ps = c.prepareStatement(sql);
             //4: Preenche os dados faltantes
-            ps.setInt(1, codigo);
+            ps.setInt(1, ID_Produto);
             //5: Executa o comando
             ps.execute();
         } catch (Exception e) {
@@ -211,65 +213,57 @@ public class processoLoja {
             ResultSet rs = ps.executeQuery();
             //5: itera sobre o resultado
             while (rs.next()) {
-                int codigo = rs.getInt("codigo");
-                String nome = rs.getString("nome");
-                String fone = rs.getString("fone");
-                String email = rs.getString("email");
+                int ID_Produto = rs.getInt("ID_Produto");
+                String nome_produto = rs.getString("nome_produto");
+                String valor = rs.getString("fone");
+                String descricao = rs.getString("email");
                 String aux = String.format(
-                        "Código: %d, Nome: %s, Fone: %s, Email: %s",
-                        codigo,
-                        nome,
-                        fone,
-                        email
+                        "Código: %d, Nome do Produto: %s, valor: %s, descricao: %s",
+                        ID_Produto,
+                        nome_produto,
+                        valor,
+                        descricao
                 );
                 JOptionPane.showMessageDialog(null, aux);
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        
-        /**
-         * 
-         * Parte de compra 
-         */
-        public void comprar(){
-            
-        }
-        
-        public void estornar(){
-            
-        }
+        }    
+    }
+    
+    /**
+     *
+     * Parte da compra
+     */
+    
+    public void compra() {
+
+    }
+
+    public void estorno() {
+
     }
     
     /**
      *
      * getters/setters
      */
-    void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    void setEmail(String email) {
-        this.email = email;
-    }
-
-    void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-    String getNome(){
-        return nome;
-    }
-    String getFone(){
-        return fone;
-    }
-    String getEmail(){
-        return email;
-    }
-    int getCodigo(){
-        return codigo;
-    }
+        
+    void setNome(String nome) {this.nome = nome;}
+    void setNome_produto(String nome_produto) {this.nome_produto = nome_produto;}
+    void setDescricao(String descricao) {this.descricao = descricao;}
+    void setSetor(String Setor) {this.Setor = Setor;}
+    void setValor(double valor) {this.valor = valor;}
+    void setCPF(int CPF) {this.CPF = CPF;}
+    void setID(int ID) {this.ID = ID;}
+    void setID_Produto(int ID_Produto) {this.ID_Produto = ID_Produto;}
+    
+    String getNome_produto() {return nome_produto;}
+    String getNome() {return nome;}
+    String getSetor() {return Setor;}
+    String getDescricao() {return descricao;}
+    int getID() {return ID;}
+    int getID_Produto() {return ID_Produto;}
+    int getCPF() {return CPF;}
+    double getValor() {return valor;}
 }
